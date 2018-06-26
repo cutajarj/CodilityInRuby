e34005651eca3a709e5d8170c1bec72251271e8b
cutajarj2 <james.cutajar@blip.pt>
2018-06-26 09:30:05 +0100

    Adding flags and maxcounters

---
+++ libs/lesson4/MaxCounters.rb
@@ -0,0 +1,24 @@
+
+def solution(n, a)
+	counters = Array.new(n, 0)
+	floor = 0
+	currentMax = 0
+	for i in a do
+		x = i - 1
+		case
+		when i > n
+			floor = currentMax
+		when counters[x] < floor
+			counters[x] = floor + 1
+		else
+			counters[x] += 1
+		end
+		currentMax = counters[x] if i <= n && counters[x] > currentMax
+	end
+	for i in 0..counters.length-1 do
+		counters[i] = floor if counters[i] < floor
+	end
+	counters
+end
+
+puts solution(5, [5]).join(",")
