e679311e8bc87ff930df9a7852fc812b3774bc67
cutajarj2 <james.cutajar@blip.pt>
2018-08-13 16:16:06 +0100

    Added solution for max profit

---
+++ libs/maximumsliceproblem/max_profit.rb
@@ -0,0 +1,18 @@
+# This is the solution for Maximum Slice Problem > Max Profit
+#
+# This is marked as PAINLESS difficulty
+
+class MaxProfit
+  def solution(a)
+    global_max_sum = 0
+    local_max_sum = 0
+    for i in 1..a.size - 1
+      d = a[i] - a[i-1]
+      local_max_sum = [d, local_max_sum + d].max
+      global_max_sum = [local_max_sum, global_max_sum].max
+    end
+    global_max_sum
+  end
+end
+
+puts MaxProfit.new.solution([23171, 21011, 21123, 21366, 21013, 21367])
