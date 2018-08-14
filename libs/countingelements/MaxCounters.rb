e809e35ab43ce9503e537ba27011a07d79df8b66
cutajarj2 <james.cutajar@blip.pt>
2018-08-14 10:32:56 +0100

    renamed some variables and comments

--- libs/countingelements/MaxCounters.rb
+++ libs/countingelements/MaxCounters.rb
@@ -5,25 +5,25 @@
 class MaxCounters
   def solution(n, a)
     counters = Array.new(n, 0)
-    floor = 0
+    start_line = 0
     current_max = 0
     for i in a do
       x = i - 1
       case
       when i > n
-        floor = current_max
-      when counters[x] < floor
-        counters[x] = floor + 1
+        start_line = current_max
+      when counters[x] < start_line
+        counters[x] = start_line + 1
       else
         counters[x] += 1
       end
       current_max = counters[x] if i <= n && counters[x] > current_max
     end
     for i in 0..counters.length - 1 do
-      counters[i] = floor if counters[i] < floor
+      counters[i] = start_line if counters[i] < start_line
     end
     counters
   end
 end
 
-puts MaxCounters.new.solution(5, [5]).join(",")
+puts MaxCounters.new.solution(5, [3, 4, 4, 6, 1, 4, 4]).join(",")
