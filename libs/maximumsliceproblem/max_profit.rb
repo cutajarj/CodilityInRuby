6666f4c927d821df11988db0eeb5c4b4af7ae31f
cutajarj2 <james.cutajar@blip.pt>
2019-01-21 13:47:55 +0000

    adding comment and refactoring for max profit

--- libs/maximumsliceproblem/max_profit.rb
+++ libs/maximumsliceproblem/max_profit.rb
@@ -7,7 +7,7 @@ class MaxProfit
     global_max_sum = 0
     local_max_sum = 0
     for i in 1..a.size - 1
-      d = a[i] - a[i-1]
+      d = a[i] - a[i - 1]
       local_max_sum = [d, local_max_sum + d].max
       global_max_sum = [local_max_sum, global_max_sum].max
     end
@@ -15,4 +15,5 @@ class MaxProfit
   end
 end
 
+#                                      -2160,   112,   243,  -353,   354
 puts MaxProfit.new.solution([23171, 21011, 21123, 21366, 21013, 21367])
