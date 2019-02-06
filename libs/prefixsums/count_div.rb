4eb950c47748ead5cd20b02c7d43ce62df307551
cutajarj2 <james.cutajar@blip.pt>
2019-02-06 15:23:56 +0000

    a slightly more simple div count

--- libs/prefixsums/count_div.rb
+++ libs/prefixsums/count_div.rb
@@ -5,8 +5,8 @@
 
 class CountDiv
   def solution(a, b, k)
-    n_start = (a.to_f/k.to_f).ceil
-    n_end = (b.to_f/k.to_f).floor
+    n_start = (a / k.to_f).ceil
+    n_end = (b / k.to_f).floor
     n_end - n_start + 1
   end
 end
