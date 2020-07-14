d1e45efd0b01d67c48043d9d2cacfd3b118f0ea7
cutajarj <james.cutajar@gmail.com>
2020-07-14 19:57:53 +0100

    changing PermMissingElem to be video friendly

--- libs/timecomplexity/perm_missing_elem.rb
+++ libs/timecomplexity/perm_missing_elem.rb
@@ -1,13 +1,16 @@
 class PermMissingElem
   def solution(a)
     max_number = a.size + 1
-    sum = 0
+    actual_sum = 0
     for number in a do
-      sum += number
+      actual_sum += number
     end
-    (max_number * (max_number + 1) / 2) - sum
+    expected_sum = (max_number * (max_number + 1) / 2)
+    expected_sum - actual_sum
   end
 end
 
 
 puts PermMissingElem.new.solution([2, 3, 1, 5])
+puts PermMissingElem.new.solution([1, 2, 3, 4, 5, 6, 7, 8, 9])
+puts PermMissingElem.new.solution([])
