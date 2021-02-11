bb76fa4a4a572ab991310efc8ff7696bf336b0bb
cutajarj <james.cutajar@gmail.com>
2021-02-11 09:03:51 +0000

    Simplifying frog river one

--- libs/countingelements/frog_river_one.rb
+++ libs/countingelements/frog_river_one.rb
@@ -7,15 +7,16 @@ class FrogRiverOne
     river_positions = Array.new(x + 1, false)
     for time in 0..a.length - 1 do
       pos = a[time]
-      if !river_positions[pos]
+      unless river_positions[pos]
         river_positions[pos] = true
         x -= 1
         return time if x == 0
       end
     end
-    return -1
+    -1
   end
 end
+
 
 puts FrogRiverOne.new.solution(5, [1, 3, 1, 4, 2, 3, 5, 4])
 
